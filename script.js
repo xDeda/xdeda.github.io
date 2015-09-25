3dff7ef701f5d487732c3445d26d954aa4de7c29
xDeda <xdedax@gmail.com>
2015-09-26 00:01:51 +0200

    much cleaner stuff
    it's in individual files and stuff now, very neat
    better secret encryption stuff (not really...)
    generally looks better

---
+++ script.js
@@ -0,0 +1,49 @@
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// ga('create', 'UA-68128014-1', 'auto');
+// ga('send', 'pageview');
+var bgcolorlist = ["#FFD3D3", "#FFE7D3", "#C9F3F3", "#CEF9CE","#C5B8FB","#9FFD9F","#81E2E2","#CD9EDC","#FFF89E","#DC8172","#FDECAD","#F58095","#D29AEF","#9C90FF","#73ADDE","#BEFBC5","#FDBE68"];
+var bgcolor = Math.floor(Math.random()*bgcolorlist.length);
+eval(atob('dmFyIHNuID0gWyJiZ28/Pz8iLCJiYWtwYW9mZ3VueiIsImxvdmUgdSBiYWJ5Iiwic2Fya2F6bSIsImhlcmUncyB0aGF0IHRoaW5nIHlvdSB3YW50ZWQsIGVwaSIsImVwaWxlcHNpIiwiaGV5IHRoZXJlLCBiZXN0YSA7LSkiLCJ3b3Jzbm9wbG92ZXIiLCJ3aG9hIHdob2Egd2hvYSB3aG9hIHdob2Egd2hvYSB3aG9hISA6LSkgaHVncyIsImltYWdpbmlzdCIsImQgZSBkIGEgbSBhIGQgZSB0IGggaSBzIiwiZGVkYSIsIm15IGZhdm91cml0ZSBub3J3ZWdpYW4hISEiLCJlbGl6YWxvdmUiLCJ5b3UgbWFrZSBtZSBkcmVhbSBvZiBidXR0ZXJmbHkgdmFsbGV5LCBteSBzd2VldGVzdCBsb3ZlIiwibWFja3N0YXIiLCJvbWcgaXQncyB0aGUgPGI+Q0hJRUYgQkVFRiBMT0NPPC9iPiIsInNpbm53aWRyaWciLCJ3ZSdsbCBzcGxpdCB0aGUgcHJvZml0cyA5NS81IiwibWlubm1hIiwiaGkgOi0tKSIsInVua25vd25jbGF3cyIsIm15IHN3ZWV0ZXN0IGFyY2hhZW9sb2dpc3QgPDMiLCJmYXJ0YmFsbGluZyIsImRlbHRyb3R0ZXIiLCJkZWV0ZWEiLCJkdSBlciBhbHRz5SBmdWNraW5nIG5pY2UiLCJmYXRieW1lYXQiLCJvLyIsImZsaW5ueWZsb3AiLCJqYWNvYiwgbXkgZGVhcmVzdCA6LSkiLCJqYWNvYm1vb2QiLCJraXNzZXMgdG8geW91IiwibGV2ZWx1cCIsIm1lIGd1c3RhcyB0dSA6LSkiLCJtYXJhY2hhb3QiLCJpIHN0aWxsIGxvdmUgdSIsIm1lcmNlcmxlcmIiLCJmcmllbmQhIiwibWluYXJ5dSIsImxpYmVydGFyaWFuaXNtIHN1Y2tzIGFzcyBkdWRlIiwibmlja25pa28iLCJpIHN3ZWFyIGknbSBnb25uYSBnZXQgYm9ybiBieSBhIHdhdGVyYmVkIHNvbWUgZGF5Iiwic3Vuc2hpcCIsInRoZSB5dW5nIHRhaHNlZW5icm8gdGhvIiwidGFoc2VlbnBybyIsInNhdCBzaXJpIGFrYWwgbGFkISIsInRodW5kZXJhaSIsImdldCBvbiB5b3VyIG1haW4gbG9sIiwicHJvcmFpIiwibWlya28gbXkgbWFpbiBtYW5nbyBtb3JuaW5nIG1hbiEiLCJsYXphYXIiLCJ5b3UncmUgaG9uZXN0bHkgbm90IGV2aWwgOnAiLCJpbmZpbml0ZWhhdGUiXTs='));
+var titles = ["0","Little Mouse","1","Shaman Disciple","2","Accomplished Shaman","3","Shaman","4","Shaman Master","5","Greedy Mouse","6","Here ! Cheese !","7","Yeeeeah Cheese ^^","8","Cheeeeeese *-*","9","Fast Mouse","10","Agile Mouse","11","Pirate Mouse","12","Ninja Mouse","13","Inspired Shaman","14","Shaman Champion","15","Glorious Shaman","16","Shaman Duchess","17","Shaman Princess","18","Shaman Empress","19","Legendary Shaman","20","Immortal Shaman","21","The Chosen Shaman","22","Holy Shaman","23","Shaman Oracle","24","Shaman Prophet","25","Shamarvelous","26","Glutton Mouse","27","Gleany","28","Plumpy Mouse","29","Paunchy Mouse","30","Chubby Mouse","31","Fluffy Mouse","32","Tubby Mouse","33","The Chubby","34","The Puffy","35","Activist Mouse","36","Unionized Mouse","37","Mouse on Strike","38","The Cheese Initiated","39","The Cheese Adept","40","The Cheese Priest","41","The Reaper","42","Rogue Mouse","43","Looter","44","Stalker","45","Frothy Mouse","46","The Silent","47","Hawk Mouse","48","Cobra Mouse","49","Spidermouse","50","Quick Silver","51","Athletic Mouse","52","Hasty Mouse","53","Rocket Mouse","54","Sonic The Mouse","55","Pingless","56","Kamikaze","57","Warrior Mouse","58","Mach 1","59","Hunter","60","First!","61","Sniper","62","Flash","63","Supermouse","64","Light Speed","65","Time Traveler","66","Fast Wind","67","E=MouseC²","68","Jumper","69","The Untouchable","70","Wall-Jumper","71","LIGHTNING","72","Cheese Finder","73","Cheese Knight","74","Cheesegrubber","75","Fatty","76","Stout Mouse","77","Cheese Lover","78","Camembert","79","Pont-L'Évêque","80","Cheese Catcher","81","It's Over 9000","82","Collector","83","Cheeseleader","84","Cheese Thief","85","Cheese Creator","86","Cheese Pizza","87","Cheese Minister","88","Prodigy Mouse","89","Princess Of Transformice","90","Cheesoholic","91","The Cheesen One","92","Sailor Mouse","93","MAH CHEESE!","94","Ancient Shaman","95","Fearless Shaman","96","Almighty Shaman","97","Architect Shaman","98","Mademoiselle","99","Lady Chamane","100","Loved","101","Magician","102","Hero of Mice","103","Angel Shaman","104","The Creator","105","Absolute Shaman","106","Miraculous Shaman","107","Liberator","108","Troll Shaman","109","Ghost Shaman","110","Spirit","111","Demigoddess Shaman","112","Last Hope","113","Redeemer","114","Alpha & Omega","115","Nice Mouse","116","Adorable Mouse","117","Charming Mouse","118","Pretty Mouse","119","Cute Mouse","120","Frivolous Mouse","121","Snob Mouse","122","Stylish Mouse","123","Actress Mouse","124","Fashion Mouse","125","Sexy","126","SuperStar","127","Little Snowflake","128","Christmas Spirit","129","Little Pixie","130","Santa Claus","136","Evian Mouse","200","Goddess Shaman","202","The Wind Master","210","Alluring Mouse ","211","Temptress","212","Latin Lover","213","Decorator","214","Builder","215","Manufacturer","216","Technician","217","Mechanic","218","Specialist","219","Inventor","220","Engineer","221","Inventive Mouse","222","Ingenious Mouse","223","Virtuoso","224","Sprinter","225","Batmouse","226","The Unseen","227","Unstoppable","228","¡Ándale! ¡Ándale!","229","Torpedo","230","Speedy Gorgonzola","231","Dynamite","232","Speedmaster","233","Whirlwind","234","Om Nom Nom","235","*-*","236","Cheese Addict","237","Cheesus","238","Queen of Cheese","240","Cookies","241","Christmas Cake","242","Whitebeard","243","Generous","244","Snowy","245","Snowstorm","246","Dauphine","247","Foxy","248","Miss Transformice","249","Omelettovore","250","My Cutie Pie","251","252","Souris Lilloise","Fiancée","253","Tonnerre de Brest","254","Chocovore","255","Chocobunny","256","Rookie","257","Neophyte","258","Private","259","Deft Mouse","260","Solo Artist","261","Corporal","262","Accurate Mouse","263","Bootcamper","264","Sergeant","265","Corner Jumper","266","Skilled Mouse","267","Lieutenant","268","Longjumper","269","Incredimouse","270","Bootcamp Addict","271","Captain","272","Dexterous Mouse","273","Maniac","274","Major","275","Cheese Artist","276","Acrobat","277","Colonel","278","Shortcutter","279","Tarzan","280","General","281","Living Legend","282","Stuntmouse","283","Pro","284","Chuck Souris","285","Queen of Bootcamp","286","Gravity Master","287","Vampire","288","Snowball","289","I'm cold","290","Grilled Chestnut","291","Chaussette","292","Souris Sapin","293","Cookies Eater","294","I Cheese You","295","Chicken","296","Explorer","297","Sardine","298","Fishermouse","299","Adorer","300","Frankenmaus","301","Candy *-*","302","Pumpkin Juice","303","Trick or Treat","304","Poltergeist","305","Halloween 2012","306","Halloween 2013","307","Von Drekkemaus","308","Candy Cane","309","Fairy Lights","310","Papaille Noel","311","Elfette","312","Snow Angel","313","Just Married","314","Candy Apple","315","Broken Heart","316","Honeymoon","317","Carnaval 2014","318","Churros","319","Partygoer","320","Cock-a-doodle-doo","321","Cocoa","322","Egg hunter","323","Childish Mouse","324","Smart","326","Mice's friend","327","Professor","328","Designer","329","Mysterious","330","Shamazing","331","Cheese Detector","332","The Brain","333","Majestic","334","Divinity","335","What the Fish","336","Fromagic","337","Yeeehaaaa!","338","Best Trainer","339","All Aboard!","340","herp derp","341","Captain Nemouse","342","Indiana Mouse","343","Casper","344","Pumpkin Pie","345","Ghostbuster","346","Zombie","347","Vampire Slayer","348","Spooky","349","Sweet Tooth","350","Wintry","351","Gingerbread","352","Polar Mouse","353","Big Turkey","354","Christmas Savior","355","Angry Mouse","356","Caramel Duchess","357","King's Pastry Chef","358","Cake Face","359","Candy Lord","360","Model Student","361","Dora the Explorer","362","Archaeologist Mouse","363","Allan Quatermouse","364","Patriotic","365","Sans-culotte","366","Dunce","367","First in the Class","368","Bookmouse","370","Namaste","440","Fromadmin","442","Sourigami","444","La Belette","445","El Pinolero","447","Kikoo Admin","448","Queijadinha","446","Dehset Admin","449","La Goulue","450","Souris Électrique"];
+
+function doBG() {
+    document.body.style.background = bgcolorlist[bgcolor];
+}
+
+function doStuff() {
+    var tinput = document.getElementById("title").value;
+    var xs = tinput.toLowerCase();
+
+    var found = [];
+	var sm;
+
+    if (xs && isNaN(xs) === false) {
+        for (var i = 0; i < titles.length; i++){
+            if (titles[i].toLowerCase().indexOf(xs) != -1){
+                found.push("<font size=1>"+titles[i]+"</font><br>"+titles[i+1]+"<br>");
+            }
+        }
+    } else {
+	
+        if (xs.length >= 2 && isNaN(xs) === true) {
+            for (var i = 0; i < titles.length; i++){
+                if (titles[i].toLowerCase().indexOf(xs) != -1){
+                    found.push("<font size=1>"+titles[i]+"</font><br> /title "+titles[i - 1]+"<br>");
+                }
+            }
+        }
+    }
+    
+    // this logic needs to be outside of loop
+    if (found.length > 0) {
+        document.getElementById("command").innerHTML = found.join("");
+    } else {
+        document.getElementById("command").innerHTML = "no results";
+    }
+
+	eval(atob('CWlmICh4cyAmJiBpc05hTih4cykgPT09IHRydWUpIHsKCQlmb3IgKHZhciBpID0gMDsgaSA8IHNuLmxlbmd0aDsgaSsrKXsKCQkJaWYgKHhzID09IHNuW2ldKXsKCQkJCXNtID0gc25baS0xXTsKCQkJfQoJCX0KCX0KCglpZiAoc20pIHsKICAgICAgICBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgic2VjcmV0IikuaW5uZXJIVE1MID0gc207CiAgICB9IGVsc2UgewogICAgICAgIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCJzZWNyZXQiKS5pbm5lckhUTUwgPSAiIjsKICAgIH0='));
+	
+}
