1842c16ab68b077c3e7a53e68e0d93d5e58aaaea
xDeda <xdedax@gmail.com>
2015-10-02 02:25:53 +0200

    added cool music

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ function link(msg) {
 	document.getElementById("title").value = msg;
 	doStuff();
 }
+function an0de() {
+	document.getElementById("command").innerHTML = '<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/149460370&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
+}
 function doStuff() {
 var tinput = document.getElementById("title").value;
 var xs = tinput.toLowerCase();
