297588857cd2136b41545bfbed4bb0f88f9e20a9
xDeda <xdedax@gmail.com>
2015-10-05 04:07:08 +0200

    smaller music

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function link(msg) {
 	doStuff();
 }
 function an0de() {
-	document.getElementById("command").innerHTML = '<iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/149460370&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
+	document.getElementById("command").innerHTML = '<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/149460370&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
 }
 function doStuff() {
 var tinput = document.getElementById("title").value;
